a673a453e6e9675b4c8ae5f4ef7f6db95e9f2515
Mustafa Kaddan <mustafakaddan@Mustafas-MacBook-Pro.local>
2023-09-19 12:23:02 +0300

    Basketball

---
+++ JS/basketball.js
@@ -0,0 +1,24 @@
+const john=[89,120,103]
+const mike=[116,94,123]
+
+let avg1=0
+let avg2=0
+let sum1=0
+let sum2=0
+
+for(i=0;i<john.length;i++)
+{
+sum1=sum1+john[i]
+sum2+=mike[i]
+}
+
+avg1=sum1/john.length
+avg2=sum2/mike.length
+
+if(avg1==avg2){
+console.log('Its a draw');}
+
+else if(avg1>avg2){
+consol.log(`John is the winner with and average point tally of ${avg1} points per game`);}
+
+else{console.log(`Mike is the winner with and average point tally of ${avg2} points per game`)}
---
+++ JS/password_5.3.js
@@ -0,0 +1,4 @@
+let password=prompt('Please insert your passowrd');
+let score=0
+
+(password.length>7)?score+=1:score=null
