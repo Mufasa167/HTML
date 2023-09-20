b1d06d1476b3784b90c7a03eccc427ee03ab3109
Mustafa Kaddan <mustafakaddan@Mustafas-MacBook-Pro.local>
2023-09-20 11:09:36 +0300

    ex_8.1

---
+++ JS/book.js
@@ -0,0 +1,16 @@
+const book={
+    Author:'Erwin Schrodinger',
+    Publisher:'Cambridge University Press',
+    Hard_cover:true,
+    Year:'2012',
+    language:'English',
+    numOfpages:184,
+    Title:'What is life?'
+
+}
+
+function defBook(book){
+    console.log(`The book "${book.Title}" was written by ${book.Author} back in 1942 and was re-published in ${book.Year}`)
+}
+
+defBook(book);
---
+++ JS/string_length.js
@@ -0,0 +1,17 @@
+const arr=['a','bb','ccc','dddd','eeeee']
+console.log(arr)
+
+let tempArr=arr.length;
+let temp=0
+temp=arr[0]
+console.log(temp)
+tempArr[0]=temp.length
+console.log(tempArr[0])
+
+/*for(i=0;i<arr.length;i++){
+    temp=arr[i];
+    console.log(temp)
+    tempArr[i]=temp.length;
+    console.log(tempArr)
+}
+*/
